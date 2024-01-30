93cc714320f562abd6f04234f5be1cbb231773e6
kimo7520 <karim.saad2327@yahoo.com>
2024-01-31 01:37:54 +0200

    provided http client module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authentication } from './interceptors/authentication.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient()],
+
 };
