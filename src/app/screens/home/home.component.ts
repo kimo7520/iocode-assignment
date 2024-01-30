5a95980eb088f711fd32ddd368534956015139fe
kimo7520 <karim.saad2327@yahoo.com>
2024-01-30 21:40:18 +0200

    added home screen

---
+++ src/app/screens/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [SearchBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  
+}
