c928b45ef707b2d6e273550b67aec55dac117897
kimo7520 <karim.saad2327@yahoo.com>
2024-01-31 01:39:33 +0200

    modified search bar componet

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { NgForOf, NgIf } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgForOf],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
-  search(movie: string){
-    console.log(movie); 
+  previousResults: string[] = [];
+  results: string[] = [];
+  isFocused = false;
+  constructor(private moviesService: MoviesService) {}
+
+  search(movie: string) {
+    console.log(movie);
+    this.previousResults.push(movie);
   }
 
-  autocomplete(ev: Event){
+  autocomplete(ev: Event) {
     const value = (ev.target as HTMLInputElement).value;
-    console.log(value);
-    
+    this.results = this.previousResults
+      .filter((result) => result.includes(value))
+      .slice(0, 10);
+    if (this.results.length < 10) {
+
+      this.moviesService.getMovies(value).pipe(
+        map(movies => {
+          return movies.results.map(movie => movie.title)
+        })
+      ).subscribe((moviesTitles) => {
+        this.results = this.results.concat(moviesTitles);
+        this.results = this.results.slice(0,10);
+      })
+    }
+  }
+
+  displayHistory(ev: Event) {
+    this.isFocused = true;
+    this.results = this.previousResults.slice(0, 10);
+    this.autocomplete(ev)
   }
 }
