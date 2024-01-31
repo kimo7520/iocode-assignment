1c50a03a9354a6c08c2bc864f9fc1fe4e746e834
kimo7520 <karim.saad2327@yahoo.com>
2024-01-31 05:40:34 +0200

    modified search bar component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,29 +1,45 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForOf, NgIf } from '@angular/common';
+import { PreviousMoviesService } from '../../services/previous-movies.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [NgIf, NgForOf],
+  imports: [NgIf, NgForOf, RouterLink],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {
-  previousResults: string[] = [];
-  filteredResults: string[] = []
+export class SearchBarComponent implements OnInit {
+  previousResults: MovieData[] = [];
+  filteredResults: MovieData[] = [];
   isFocused = false;
   @Output() movieEmitter = new EventEmitter<string>();
-  search(movie: string) {
-    // alert(movie);
-    this.previousResults.unshift(movie);
+
+  constructor(private previousMoviesService: PreviousMoviesService) {}
+
+  ngOnInit(): void {
+    this.previousMoviesService.previousMovies$
+      .asObservable()
+      .subscribe((movie) => {
+        if(movie){
+          this.previousResults.unshift({
+            id: movie.id,
+            title: movie.title,
+          });
+
+        }
+      });
   }
 
   autocomplete(ev: Event) {
+    console.log(this.previousResults);
+    
     const value = (ev.target as HTMLInputElement).value;
     this.movieEmitter.emit(value);
-    
+
     this.filteredResults = this.previousResults
-      .filter((result) => result.startsWith(value))
+      .filter((result) => result.title?.startsWith(value))
       .slice(0, 10);
   }
 
@@ -32,4 +48,9 @@ export class SearchBarComponent {
     this.filteredResults = this.previousResults.slice(0, 10);
     this.autocomplete(ev);
   }
+}
+
+interface MovieData {
+  title: string;
+  id: number;
 }
