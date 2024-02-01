fca4d799415b29e5603d0c57ca438cc0b1691a6a
kimo7520 <karim.saad2327@yahoo.com>
2024-02-01 05:40:56 +0200

    provided http client & animation  modules

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authentication } from './interceptors/authentication.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()],
 
 };
