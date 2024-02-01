f864a6a8b54ead8016e42083c78fb7d933d43e9e
kimo7520 <karim.saad2327@yahoo.com>
2024-02-01 05:27:35 +0200

    fetched data into overview component && added exporting

--- src/app/screens/overview/overview.component.ts
+++ src/app/screens/overview/overview.component.ts
@@ -5,15 +5,22 @@ import { ActivatedRoute } from '@angular/router';
 import { MovieOverview } from '../../models/movie-overview.model';
 import * as XLSX from 'xlsx';
 import { HelperService } from '../../services/helper.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { RemoveUndescorePipe } from '../../pipes/remove-undescore.pipe';
+interface cloumns {
+  item: string;
+  value: string;
+}
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule, RemoveUndescorePipe],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
 export class OverviewComponent implements OnInit {
-  overview?: MovieOverview;
+  overview!: MovieOverview;
 
   constructor(
     public moviesService: MoviesService,
@@ -25,16 +32,37 @@ export class OverviewComponent implements OnInit {
     const movieId = this.activatedRoute.snapshot.paramMap.get('id');
     this.moviesService
       .getMovie(movieId!)
-      .subscribe((overview) => (this.overview = overview));
+      .subscribe((overview) => {
+        this.overview = overview
+        this.datasource = Object.keys(overview).filter(key => {
+          return !Array.isArray(overview[key as keyof typeof overview]) && typeof overview[key as keyof typeof overview] !== 'object'
+        }).map(key => {
+          return {
+            item: key,
+            value: String(overview[key as keyof typeof overview])
+          }
+        })
+        console.log(this.datasource);
+        
+      });
   }
 
-  export(){
-    const overview = this.helper.flattenObject(this.overview);    
+  export() {
+    const overview = this.helper.flattenObject(this.overview);
     const worksheet = XLSX.utils.json_to_sheet(overview);
     const workbook: XLSX.WorkBook = {
-      Sheets: {data: worksheet},
-      SheetNames: ['data']
+      Sheets: { data: worksheet },
+      SheetNames: ['data'],
     };
     XLSX.writeFile(workbook, 'movie.xlsx');
   }
+  displayedColumns: string[] = ['item', 'value'];
+  datasource: cloumns[] = [];
+
+  /** Gets the total cost of all transactions. */
+
+
+ 
+ 
+
 }
